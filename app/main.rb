0fb59dbc321db1f92d027504fcf21b8d59596384
danielwells <danielwells.usnret@gmail.com>
2014-03-13 16:27:37 -0500

    Add page background color, header/footer placeholders

--- app/main.rb
+++ app/main.rb
@@ -4,7 +4,9 @@ require_relative './functions'
 
 class PLBlog < Sinatra::Base
   # NOTE: If have to embed app root directory in a directory string, use
-  #       "#{ToDoList.settings.root}/...rest of directory structure"
+  #       "#{<application's sinatra class>.settings.root}/...rest of directory structure"
+  #  Example code:
+  #    @script = Script.new("#{MadLib.settings.root}/scripts/script1.txt")
   
   # Helper methods
   helpers do
@@ -13,13 +15,25 @@ class PLBlog < Sinatra::Base
 
   # Setup/common methods for all routes
   before do
-    #Example code:
-    #    @script = Script.new("#{MadLib.settings.root}/scripts/script1.txt")
+
   end
 
   # Routes methods
   get "/" do
 
+    erb :bloghome
+  end
+  
+  get "/search" do
+    
+  end
+  
+  get "/about" do
+    
+  end
+  
+  get "/contact_info" do
+    
   end
 
 end
