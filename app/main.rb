479dff666caaa690df652028a4980e68405ef9aa
danielwells <danielwells.usnret@gmail.com>
2014-03-16 16:56:32 -0500

    Corrected 'LIKE' query string for section images so that it would distinguish between 'JavaScript' and 'Java'.

--- app/main.rb
+++ app/main.rb
@@ -67,13 +67,11 @@ class PLBlog < Sinatra::Base
     end
     
     def add_code_snippet_to_section(link, s) 
-      binding.pry
       if Image.find_by(section_id: s.id, img_type: "snippet", img_link: link).nil?    
         s.images.create({
           :img_type => "snippet",
           :img_link => link}) 
       end     
-      binding.pry
     end
             
   end
@@ -91,10 +89,9 @@ class PLBlog < Sinatra::Base
     languages = ["JavaScript", "Java", "PHP", "C#", "Python", "C/C++", "Ruby", "Objective-C"]
     add_language_sections_to_article(languages, @current_article)
     
-    js_sec = @current_article.sections.where("section_title LIKE ?", "%#{languages[0]}%")[0]
+    js_sec = @current_article.sections.where("section_title LIKE ?", "%(#{languages[0]})%")[0]
     add_code_snippet_to_section("http://i.imgur.com/8WFZtua.png", js_sec)
-    add_code_snippet_to_section("http://i.imgur.com/sTUF2EC.png", js_sec)
-    java_sec = @current_article.sections.where("section_title LIKE ?", "%#{languages[1]}%")[0]
+    java_sec = @current_article.sections.where("section_title LIKE ?", "%(#{languages[1]})%")[0]
     add_code_snippet_to_section("http://i.imgur.com/sTUF2EC.png", java_sec)
     
 
